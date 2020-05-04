9452c646900c65a81e14fc8286105d754d8d2201
Bertrand Fournel <fournel.bertrand@gmail.com>
2020-05-04 18:03:00 +0200

    rajout des parc relais

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = () => {
 
     let parkings = document.getElementById('list-parkings').childNodes
+    let parcRelais = document.getElementById('list-parcrelais').childNodes
+    
     let canvas = []
     let contexts = []
     
@@ -11,8 +13,15 @@ window.onload = () => {
         const context = element.getContext('2d')
         contexts.push(context)
     }
-
-    const url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=export-api-parking-citedia"        
+    for (let i = 1; i < parcRelais.length; i += 2) {
+        const element = parcRelais[i]
+        canvas.push(element)
+        const context = element.getContext('2d')
+        contexts.push(context)
+    }
+    
+    const urlParkings = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=export-api-parking-citedia"
+    const urlParcRelais = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-parcs-relais-du-reseau-star-en-temps-reel&sort=idparc&facet=nom&facet=etat"       
     
     let totalPlaces = []
     let totalLibres = []
@@ -21,7 +30,7 @@ window.onload = () => {
 
     function app(){
         getData().then(() =>{
-        for (let i = 0; i <= 9; i++) {
+        for (let i = 0; i <= 14; i++) {
             charIt(contexts[i], labelsParking[i], totalPlaces[i], totalLibres[i],status[i]).then(()=>{
                 canvas.forEach(element =>{
                     element.style.display = "inline-block"
@@ -37,7 +46,7 @@ window.onload = () => {
 
     // On dessine les graphiques avec les données récupérées sur chacun des canvas, en vérifiant si le parking est ouvert.
     async function charIt(context, label, maxPlace, freePlace, status){
-        if (status == "OUVERT"){
+        if (status == "OUVERT" || status == "Ouvert"){
             var myChart = new Chart(context, {
                 type: 'doughnut',
                 data: {
@@ -99,34 +108,64 @@ window.onload = () => {
     }
     // On récupère les données avec cette fonction que l'on placera dans des arrays
     async function getData(){
-        const response =  await fetch(url)
-        const data = await response.json()
+        const responseParking =  await fetch(urlParkings)
+        const dataParkings = await responseParking.json()
+
+        const responseParcRelais =  await fetch(urlParcRelais)
+        const dataParcRelais = await responseParcRelais.json()
 
 
         //On récupère les noms des parkings
         for (let i = 0; i <= 9; i++) {
-            const name = data.records[i].fields.key
+            const name = dataParkings.records[i].fields.key
             labelsParking.push(name)
         }
 
 
-        // On récupère les places totales
+        // On récupère les places totales des parkings
         for (let i = 0; i <= 9; i++) {
-            const maxPlaces = data.records[i].fields.max
+            const maxPlaces = dataParkings.records[i].fields.max
             totalPlaces.push(maxPlaces)
         }
 
-        // On récupère les places libres
+        // On récupère les places libres des parkings
         for (let i = 0; i <= 9; i++) {
-            const freePlaces = data.records[i].fields.free
+            const freePlaces = dataParkings.records[i].fields.free
             totalLibres.push(freePlaces)
         }
         
         
-        // On récupère le status (ouvert ou fermé)
+        // On récupère le status (ouvert ou fermé) des parkings
         for (let i = 0; i <= 9; i++) {
-            const statut = data.records[i].fields.status
+            const statut = dataParkings.records[i].fields.status
             status.push(statut)
-        } 
+        }
+
+        //On récupère les noms des Parc Relais
+        for (let i = 0; i <= 4; i++) {
+            const name = dataParcRelais.records[i].fields.nom
+            labelsParking.push(name)
+        }
+
+
+        // On récupère les places totales des Parc Relais
+        for (let i = 0; i <= 4; i++) {
+            const maxPlaces = dataParcRelais.records[i].fields.capaciteactuelle
+            totalPlaces.push(maxPlaces)
+        }
+
+        // On récupère les places libres des Parc Relais
+        for (let i = 0; i <= 4; i++) {
+            const freePlaces = dataParcRelais.records[i].fields.nombreplacesdisponibles
+            totalLibres.push(freePlaces)
+        }
+        
+        
+        // On récupère le status (ouvert ou fermé) des Parc Relais
+        for (let i = 0; i <= 4; i++) {
+            const statut = dataParcRelais.records[i].fields.etat
+            status.push(statut)
+        }
+        console.log(labelsParking)
     }
 }
